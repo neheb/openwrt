4b6a140de67f936625c6165f999bebb3f6630d0c
Rosen Penev <rosenp@gmail.com>
2022-09-15 15:54:13 -0700

    mac80211.sh: remove ==
    == is a bashism. Found with shellcheck's SC3045
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- package/network/config/wifi-scripts/files/lib/netifd/wireless/mac80211.sh
+++ package/network/config/wifi-scripts/files/lib/netifd/wireless/mac80211.sh
@@ -1058,7 +1058,7 @@ get_freq() {
 	iw "$phy" info | awk -v band="$band" -v channel="[$channel]" '
 
 $1 ~ /Band/ {
-	band_match = band == $2
+	band_match = band = $2
 }
 
 band_match && $3 == "MHz" && $4 == channel {
